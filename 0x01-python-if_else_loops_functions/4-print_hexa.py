210d31f576eaf3d32fad7a7ca7967057f67e99c4
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-03-07 14:19:17 +0200

    vier

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for num in range(99):
-    print("{:d} = {:s}".format(num, hex(num)))
+# Author - Godswill Kalu
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
