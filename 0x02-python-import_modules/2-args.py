b23baf5f47efe013b58d7a390a22ef884c631d09
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-03-09 07:53:49 +0200

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-args.py
-# Manuel Dinis Júnior <manueldinisjunior.com>
+# Manuel Dinis Júnior <manueldinisjunior@gmail.com>
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
