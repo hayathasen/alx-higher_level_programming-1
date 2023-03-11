b0f7f6914679843c805762cde6f9f92ecc5f1abd
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-03-11 09:49:19 +0200

    Python Data Structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
-    """
-    find the maximum value of a list
-    Args:
-        my_list - list to search
-    Return:
-        None - if list is empty
-        maximum of list
-    """
-    L = len(my_list)
-    if L == 0:
+    if my_list:
+        biggest = my_list[0]
+        for i in my_list:
+            if i > biggest:
+                biggest = i
+        return biggest
+    else:
         return None
-    Max = my_list[0]
-    for elem in my_list:
-        if elem > Max:
-            Max = elem
-    return 
